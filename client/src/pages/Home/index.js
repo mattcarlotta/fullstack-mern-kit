762bb2bcfffb49c9a70baa7940f1b4a58c2a8667
Matt <carlotta.matt@gmail.com>
2019-06-06 15:23:00 -0700

    Fix home page message

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -15,7 +15,7 @@ const Home = () => (
     <Helmet title="Home" />
     <div className={logoContainer}>
       <img className={logoStyle} src={logo} alt="mernLogo.png" />
-      <h1 className={textInfo}>React SSR Kit</h1>
+      <h1 className={textInfo}>React FullStack MERN Kit</h1>
       <h1 className={textInfo}>Edit files in ./src and save to reload.</h1>
       <Link style={{ marginTop: 10 }} to="/users">
         <ExampleIcon />
